6951cc6a7e206ff03a5029eec0d5a760af1051e8
Shendrit Kqiku <shenndritkqiku@gmail.com>
2021-05-03 23:33:33 +0200

    Fix Reservation validations

--- RentalCar/Controllers/ReservationController.cs
+++ RentalCar/Controllers/ReservationController.cs
@@ -1,182 +1,218 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Models;
 using ViewModels;
 
-namespace Controllers
+namespace RentalCar.Controllers
 {
-    [Authorize]
-    public class ReservationController : Controller
-    {
-        private readonly DataContext _context;
-        public ReservationController(DataContext context)
-        {
-            this._context = context;
-        }
+   [Authorize]
+   public class ReservationController : BaseController
+   {
+      private readonly UserManager<AppUser> _userManager;
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var reservations = await _context.Reservations
-               .Include(r => r.User)
-               .Include(r => r.Vehicle)
-               .ToListAsync();
+      public ReservationController(DataContext context, UserManager<AppUser> userManager)
+      {
+         this._context = context;
+         this._userManager = userManager;
+      }
+      private readonly DataContext _context;
 
-            return View(reservations);
-        }
+      [HttpGet]
+      public async Task<IActionResult> Index()
+      {
+         var currentUserId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+         var user = await _userManager.FindByIdAsync(currentUserId);
+         if (user is null)
+         {
+            return RedirectToAction("Main", "Home");
+         }
 
-        [HttpGet("{reservationId}")]
-        public async Task<IActionResult> Details(int reservationId)
-        {
-            var reservation = await _context.Reservations
-               .Include(r => r.User)
-               .Include(r => r.Vehicle)
-               .FirstOrDefaultAsync(r => r.Id == reservationId);
+         var reservations = _context.Reservations.AsQueryable();
 
-            if (reservation is null)
+         // If not admin, filter only reservations of this user
+         if (!(await _userManager.IsInRoleAsync(user, "Admin")))
+         {
+            reservations = reservations
+                           .Where(r => r.UserId == user.Id);
+         }
+
+         reservations = reservations
+                       .Include(r => r.User)
+                       .Include(r => r.Vehicle);
+
+         return View(await reservations.ToListAsync());
+      }
+
+      [HttpGet("{reservationId}")]
+      public async Task<IActionResult> Details(int reservationId)
+      {
+         var reservation = await _context.Reservations
+            .Include(r => r.User)
+            .Include(r => r.Vehicle)
+            .FirstOrDefaultAsync(r => r.Id == reservationId);
+
+         if (reservation is null)
+         {
+            return RedirectToAction("Index");
+         }
+
+         return View(reservation);
+      }
+
+      [HttpGet]
+      public async Task<IActionResult> Create()
+      {
+         var vehicles = await _context.Vehicles.ToListAsync();
+         ViewBag.Vehicles = vehicles;
+         return View();
+      }
+
+      [HttpPost]
+      public async Task<IActionResult> Create(ReservationViewModel reservationViewModel)
+      {
+         if (ModelState.IsValid)
+         {
+            // ReservedTo is before ReservedFrom
+            if (reservationViewModel.ReservedFrom.ToUniversalTime()
+               .CompareTo(reservationViewModel.ReservedTo.ToUniversalTime()) > 0)
             {
-                return RedirectToAction("Index");
+               ModelState.AddModelError(string.Empty, "Life can only be understood backwards; but it must be lived forwards. (i.e pick a future date for the reservation)");
+
+               var vehicles = await _context.Vehicles.ToListAsync();
+               ViewBag.Vehicles = vehicles;
+               return View();
             }
 
-            return View(reservation);
-        }
+            // Get latest reservation
+            var latestReservation = await _context.Reservations
+                  .OrderByDescending(r => r.Id)
+                  .Take(1)
+                  .FirstAsync();
 
-        [HttpGet]
-        public async Task<IActionResult> Create()
-        {
-            var vehicles = await _context.Vehicles.ToListAsync();
-            ViewBag.Vehicles = vehicles;
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(ReservationViewModel reservationViewModel)
-        {
-            if (ModelState.IsValid)
+            // Check if dates collide
+            if (reservationViewModel.ReservedFrom.ToUniversalTime()
+                .CompareTo(latestReservation.ReservedTo) < 0)
             {
-                // Get latest reservation
-                //  var latestReservation = await _context.Reservations
-                //        .OrderByDescending(r => r.Id)
-                //        .Take(1)
-                //        .FirstAsync();
+               ModelState.AddModelError(string.Empty, $"This vehicle is already reserved. Please specify a date later than '{latestReservation.ReservedTo.ToUniversalTime()}'");
 
-                //  // Check if dates collide
-                //  if (reservationViewModel.ReservedFrom.ToUniversalTime().CompareTo(latestReservation.ReservedTo.ToUniversalTime()) < 0)
-                //  {
-                //      ModelState.AddModelError(string.Empty, $"This vehicle is already reserved. Please specify a date later than '{latestReservation.ReservedTo.ToUniversalTime().ToShortDateString()}'");
-                //      return View();
-                //  }
-
-                var reservation = new Reservation
-                {
-                    ReservedFrom = reservationViewModel.ReservedFrom.ToUniversalTime(),
-                    ReservedTo = reservationViewModel.ReservedTo.ToUniversalTime(),
-                    VehicleId = reservationViewModel.VehicleId,
-                    UserId = reservationViewModel.UserId,
-                    DeliveryAddress = reservationViewModel.DeliveryAddress,
-                    NumberOfDays = (reservationViewModel.ReservedTo.ToUniversalTime().Date - reservationViewModel.ReservedFrom.ToUniversalTime().Date).Days > 0 ? (reservationViewModel.ReservedTo.ToUniversalTime().Date - reservationViewModel.ReservedFrom.ToUniversalTime().Date).Days : 1,
-                };
-
-                _context.Reservations.Add(reservation);
-                var success = await _context.SaveChangesAsync() > 0;
-
-                if (success)
-                {
-                    return RedirectToAction("Index");
-                }
+               var vehicles = await _context.Vehicles.ToListAsync();
+               ViewBag.Vehicles = vehicles;
+               return View();
             }
 
-            return View();
-        }
-
-        [HttpGet("/EditReservation/{reservationId}")]
-        public async Task<IActionResult> Edit(int reservationId)
-        {
-            var reservationInfo = await _context.Reservations
-               .Include(r => r.User)
-               .Include(r => r.Vehicle)
-               .FirstOrDefaultAsync(r => r.Id == reservationId);
-
-            var vehicles = await _context.Vehicles.ToListAsync();
-            ViewBag.Vehicles = vehicles;
-
-            if (reservationInfo is null)
+            var reservation = new Reservation
             {
-                return RedirectToAction("Index");
-            }
+               ReservedFrom = reservationViewModel.ReservedFrom.ToUniversalTime(),
+               ReservedTo = reservationViewModel.ReservedTo.ToUniversalTime(),
+               VehicleId = reservationViewModel.VehicleId,
+               UserId = reservationViewModel.UserId,
+               DeliveryAddress = reservationViewModel.DeliveryAddress,
+               NumberOfDays = (reservationViewModel.ReservedTo.ToUniversalTime().Date - reservationViewModel.ReservedFrom.ToUniversalTime().Date).Days > 0 ? (reservationViewModel.ReservedTo.ToUniversalTime().Date - reservationViewModel.ReservedFrom.ToUniversalTime().Date).Days : 1,
+            };
 
-            return View(reservationInfo);
-        }
+            _context.Reservations.Add(reservation);
+            var success = await _context.SaveChangesAsync() > 0;
 
-
-        [Authorize(Roles = "Admin")]
-        [HttpPost("{reservationId}")]
-        public async Task<IActionResult> Edit(int reservationId, ReservationEditViewModel reservationViewModel)
-        {
-            ModelState.Remove("VehicleId");
-
-            if (ModelState.IsValid)
+            if (success)
             {
-                var reservation = await _context.Reservations
-                    .FirstOrDefaultAsync(r => r.Id == reservationId);
-
-                if (reservation is null)
-                {
-                    return RedirectToAction("Index");
-                }
-
-                if (reservation.ReservedTo.ToUniversalTime().CompareTo(DateTime.UtcNow) > 0)
-                {
-                    reservation.ReservedTo = reservationViewModel.ReservedTo;
-                }
-
-                if (!string.IsNullOrEmpty(reservationViewModel.DeliveryAddress))
-                {
-                    reservation.DeliveryAddress = reservationViewModel.DeliveryAddress;
-                }
-
-                if (reservationViewModel.NumberOfDays > 0)
-                {
-                    reservation.NumberOfDays = reservationViewModel.NumberOfDays;
-                }
-
-                var success = await _context.SaveChangesAsync() > 0;
-
-                if (success)
-                {
-                    return RedirectToAction("Details", new { reservationId = reservationId });
-                }
+               return RedirectToAction("Index");
             }
+         }
 
-            return View();
-        }
+         return View();
+      }
 
-        [Authorize(Roles = "Admin")]
-        [HttpPost()]
-        public async Task<IActionResult> Delete(int reservationId)
-        {
+      [HttpGet("/EditReservation/{reservationId}")]
+      public async Task<IActionResult> Edit(int reservationId)
+      {
+         var reservationInfo = await _context.Reservations
+            .Include(r => r.User)
+            .Include(r => r.Vehicle)
+            .FirstOrDefaultAsync(r => r.Id == reservationId);
+
+         var vehicles = await _context.Vehicles.ToListAsync();
+         ViewBag.Vehicles = vehicles;
+
+         if (reservationInfo is null)
+         {
+            return RedirectToAction("Index");
+         }
+
+         return View(reservationInfo);
+      }
+
+
+      [Authorize(Roles = "Admin")]
+      [HttpPost("{reservationId}")]
+      public async Task<IActionResult> Edit(int reservationId, ReservationEditViewModel reservationViewModel)
+      {
+         ModelState.Remove("VehicleId");
+
+         if (ModelState.IsValid)
+         {
             var reservation = await _context.Reservations
                 .FirstOrDefaultAsync(r => r.Id == reservationId);
 
             if (reservation is null)
             {
-                return RedirectToAction("Index");
+               return RedirectToAction("Index");
             }
 
-            _context.Reservations.Remove(reservation);
+            if (reservation.ReservedTo.ToUniversalTime().CompareTo(DateTime.UtcNow) > 0)
+            {
+               reservation.ReservedTo = reservationViewModel.ReservedTo;
+            }
+
+            if (!string.IsNullOrEmpty(reservationViewModel.DeliveryAddress))
+            {
+               reservation.DeliveryAddress = reservationViewModel.DeliveryAddress;
+            }
+
+            if (reservationViewModel.NumberOfDays > 0)
+            {
+               reservation.NumberOfDays = reservationViewModel.NumberOfDays;
+            }
+
             var success = await _context.SaveChangesAsync() > 0;
 
             if (success)
             {
-                return RedirectToAction("Index");
+               return RedirectToAction("Details", new { reservationId = reservationId });
             }
+         }
 
-            return View();
-        }
-    }
+         return View();
+      }
+
+      [Authorize(Roles = "Admin")]
+      [HttpPost()]
+      public async Task<IActionResult> Delete(int reservationId)
+      {
+         var reservation = await _context.Reservations
+             .FirstOrDefaultAsync(r => r.Id == reservationId);
+
+         if (reservation is null)
+         {
+            return RedirectToAction("Index");
+         }
+
+         _context.Reservations.Remove(reservation);
+         var success = await _context.SaveChangesAsync() > 0;
+
+         if (success)
+         {
+            return RedirectToAction("Index");
+         }
+
+         return View();
+      }
+   }
 }
